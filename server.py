a1c8109e120c44b3aa2eae1a84c39589ab97a638
Joel Lonbeck <joel@neutrinographics.com>
2014-09-11 14:51:09 -0700

    updated samples, added better error handling to server

--- server.py
+++ server.py
@@ -1,27 +1,57 @@
 import logging
+import sys
 from twisted.internet import protocol, reactor, endpoints
 import json
+import os.path
 
-# load settings from the config file
-json_data = open('config.json')
-data = json.load(json_data)
-json_data.close()
+# global variables
+api_version = "1.0.0"
+user_key_path = ""
+device_key_path = ""
+port = ""
 
-user_key_path = data['key_path']['user']
-device_key_path = data['key_path']['user']
+# reads in the config file
+def loadConfig(config_path):
+    global user_key_path, device_key_path, port
+
+    if not os.path.isfile(config_path):
+        print("the configuration file is missing")
+        exit()
+
+    # load settings from the config file
+    json_data = open(config_path)
+    try:
+        data = json.load(json_data)
+    except ValueError:
+        print("the configuration file could not be parsed")
+        exit()
+    json_data.close()
+
+    # validate configuration
+    if 'port' not in data or 'key_path' not in data or 'user' not in data['key_path'] or 'device' not in data['key_path']:
+        print("missing some configuration details")
+        exit()
+
+    user_key_path = str(data['key_path']['user'])
+    device_key_path = str(data['key_path']['user'])
+    port = str(data['port'])
+
+    # validate configuration values
+    if user_key_path == "" or device_key_path == "" or port == "":
+        print("invalid configuration values")
+        exit()
 
 class ResponseHandler(protocol.Protocol):
-    global user_key_path, device_key_path
-    apiVersion = '1.0.0'
+    global user_key_path, device_key_path, api_version
 
     # sends an error message to the client
     def sendError(self, message):
-        response = '{"version":"'+self.apiVersion+'","error":"'+message+'"}'
+        response = '{"version":"'+api_version+'","error":"'+message+'"}'
         self.transport.write(response)
 
     # sends a ok message to the client
     def sendOk(self, message):
-        response = '{"version":"'+self.apiVersion+'","ok":"'+message+'"}'
+        response = '{"version":"'+api_version+'","ok":"'+message+'"}'
         self.transport.write(response)
 
     # handle responses
@@ -30,19 +60,28 @@ class ResponseHandler(protocol.Protocol):
         try:
             data = json.loads(json_data)
         except ValueError:
+            print "Unexpected error:", sys.exc_info()[0]
             self.sendError('invalid request')
             return
 
-        if data['key'] is None or data['udid'] is None:
+        if 'key' not in data or data['key'] == "" or 'udid' not in data or data['udid'] == "":
             self.sendError('incomplete request')
         else:
-            print('key\n'+data['key']+'\n\n'+data['udid']+'\n\n')
-            # process key and UUID
+            print('key\n'+data['key']+'\n\nudid\n'+data['udid']+'\n\n')
+            # TODO: process key and UUID
             self.sendOk('done')
 
 class ResponseFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return ResponseHandler()
 
-endpoints.serverFromString(reactor, "tcp:1234").listen(ResponseFactory())
+loadConfig("config.json")
+
+print('--------------------------')
+print('Translation Studio Server')
+print('Version: '+api_version)
+print('Listening on port: '+port)
+print('--------------------------\n')
+
+endpoints.serverFromString(reactor, 'tcp:'+port).listen(ResponseFactory())
 reactor.run()
