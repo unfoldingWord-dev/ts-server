45f88005be14ed77cea75aab907d90866949945f
Joel Lonbeck <joel@neutrinographics.com>
2014-09-11 12:55:51 -0700

    added key and udid splitting

--- README.md
+++ README.md
@@ -11,9 +11,19 @@ To test on your local environment you'll need a tool that can communicate with a
 Telnet is available on OSX by default, but you'll need to 
 [manually enable telnet](http://technet.microsoft.com/en-us/library/cc771275(v=ws.10).aspx) it if using Windows 7.
 
-># connect to the port
+>\# connect to the port
 >telnet 127.0.0.1 [port]
-># send a message
+>\# send a message
 >hello world
+
+###Usage
+The script expects to receive data in the form of `key:UDID` where `key` is the public ssh key of the mobile device
+and `UDID` is the unique device id. All data should be sent as a single line string.
+
+####Responses
+The server will respond with a `1` for success and `0` for errors. All responses are prepended with the api version as well.
+
+For example successful communication with the server that is running api version `1.0.0` will look like the following.
+>1.0.0:1
 
 [Twisted]:https://twistedmatrix.com/trac/
--- server.py
+++ server.py
@@ -4,12 +4,26 @@ from twisted.internet import protocol, reactor, endpoints
 class ResponseHandler(protocol.Protocol):
     # response types
     responses = {'ok':'1', 'error':'0'}
+    apiVersion = '1.0.0'
+
+    # formats the response message
+    def sendResponse(self, message):
+        # include the server api version
+        response = self.apiVersion+':'+message
+        self.transport.write(response)
 
     # handle responses
     def dataReceived(self, data):
-        # receive public key
-        print(data)
-        self.transport.write(self.responses['ok'])
+        # receive public key. Expects key:UDID
+        pieces = data.split(':')
+        if len(pieces) != 2:
+            self.sendResponse(self.responses['error'])
+        else:
+            key = pieces[0]
+            uuid = pieces[1]
+            print('key\n'+key+'\n\n'+uuid+'\n\n')
+            # process key and UUID
+            self.sendResponse(self.responses['ok'])
 
 class ResponseFactory(protocol.Factory):
     def buildProtocol(self, addr):
